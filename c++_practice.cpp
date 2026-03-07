5e78e092affe7753907ec82c0a51374ebda4bce9
Heewon Yang <happyeone25@pusan.ac.kr>
2026-03-07 12:02:37 +0900

    update code

--- c++_practice.cpp
+++ c++_practice.cpp
@@ -6,14 +6,6 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int N,T,a,b;
-    cin >> N;
-    vector<int> v(6);
-    for (int i=0; i<6; i++) {
-        cin >> v[i];
-    }
-    sort(v.begin(),v.end());
-    T = max(v);
-    N / P = 
+    cout << "Helo World" << endl;
     return 0;
 }
